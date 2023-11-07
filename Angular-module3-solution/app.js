c0876e83afaec01ebca5763665a79532c044107e
VickyChen0623 <96273804+VickyChen0623@users.noreply.github.com>
2023-11-06 22:55:19 -0500

    angular module 3 setup

---
+++ Angular-module3-solution/app.js
@@ -0,0 +1,14 @@
+ (function () {
+    'use strict';
+    angular.module('NarrowItDownApp', [])
+        .controller('NarrowItDownController ', NarrowItDownController)
+        .service('MenuSearchService', MenuSearchService);
+
+     function NarrowItDownController () {
+
+     }
+
+     function MenuSearchService () {
+
+     }
+ })();
