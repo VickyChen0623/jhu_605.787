503201d2f07348708f2f678543b7f7deb2e99bad
VickyChen0623 <96273804+VickyChen0623@users.noreply.github.com>
2023-11-07 23:25:12 -0500

    module 3 solution

--- Angular-module3-solution/app.js
+++ Angular-module3-solution/app.js
@@ -1,14 +1,64 @@
  (function () {
     'use strict';
     angular.module('NarrowItDownApp', [])
-        .controller('NarrowItDownController ', NarrowItDownController)
-        .service('MenuSearchService', MenuSearchService);
+        .controller('NarrowItDownController', NarrowItDownController)
+        .service('MenuSearchService', MenuSearchService)
+        .directive('foundItems', FoundItems);
 
-     function NarrowItDownController () {
+    NarrowItDownController.$inject = ['MenuSearchService'];
+    MenuSearchService.$inject = ['$http'];
 
+     function NarrowItDownController (MenuSearchService) {
+        var narrow = this;
+
+         narrow.searchAndFilter = function (searchTerm) {
+             if (searchTerm === "") {
+                 narrow.found = [];
+                 return
+             }
+            MenuSearchService.getMatchedMenuItems(searchTerm).then(function (foundItems) {
+                narrow.found = foundItems;
+            })
+        }
+        narrow.removeItem = function (itemIndex) {
+             narrow.found.splice(itemIndex, 1);
+        }
      }
 
-     function MenuSearchService () {
+     function MenuSearchService ($http) {
+        var service = this;
+
+        service.getMatchedMenuItems = function(searchTerm) {
+            return $http({
+                method: 'GET',
+                url: 'https://coursera-jhu-default-rtdb.firebaseio.com/menu_items.json'
+            }).then(function (response) {
+                const data = response.data;
+                var allItems = [];
+                for (const key in data) {
+                    allItems.push(...data[key].menu_items);
+                }
+                return allItems.filter(function (item) {
+                    return item.description.indexOf(searchTerm) > -1;
+                });
+            })
+        }
+     }
+     function FoundItems () {
+         var ddo = {
+             templateUrl: 'foundItemList.html',
+             scope: {
+                 items: '<',
+                 onRemove: '&'
+             },
+             controller: FoundItemsController,
+             controllerAs: 'foundItems',
+             bindToController: true
+         }
+
+         return ddo;
+     }
+     function FoundItemsController () {
 
      }
  })();
