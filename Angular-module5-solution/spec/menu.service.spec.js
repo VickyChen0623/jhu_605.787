fc01067c1a9c64f8718a7fb04ffce7e61c5ea428
VickyChen0623 <96273804+VickyChen0623@users.noreply.github.com>
2023-12-10 20:08:31 -0500

    added test case returning null

--- Angular-module5-solution/spec/menu.service.spec.js
+++ Angular-module5-solution/spec/menu.service.spec.js
@@ -15,14 +15,13 @@ describe('menuService', function () {
   });
 
   it('should return menu item', async function() {
-    $httpBackend.whenGET('https://coursera-jhu-default-rtdb.firebaseio.com' + '/menu_items/L/menu_items/1.json').respond({
+    $httpBackend.whenGET('https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/L/menu_items/1.json').respond({
       "description": "chunks of chicken, breaded and deep-fried with sauce and scallions; white meat by request: for pint $1 extra, for large $2 extra",
       "name": "General Tso's Chicken",
       "price_large": 9.75,
       "short_name": "L2"
     });
     const response = await menuService.getMenuItem("L1");
-    console.log('response:  ', response)
     expect(response.data).toEqual({
       "description": "chunks of chicken, breaded and deep-fried with sauce and scallions; white meat by request: for pint $1 extra, for large $2 extra",
       "name": "General Tso's Chicken",
@@ -32,4 +31,10 @@ describe('menuService', function () {
     $httpBackend.flush();
   });
 
+  it('should not return menu item', async function() {
+    $httpBackend.whenGET('https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/L/menu_items/1.json').respond(null);
+    const response = await menuService.getMenuItem("L30");
+    expect(response.data).toEqual(null);
+    $httpBackend.flush();
+  });
 });
